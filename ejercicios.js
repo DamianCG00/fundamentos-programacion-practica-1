d7a2106ed751253e073ee142ecad7b27614a3340
DamianCG00 <marco.coba110107@gmail.com>
2025-11-05 12:59:28 -0600

    test

--- ejercicios.js
+++ ejercicios.js
@@ -15,9 +15,9 @@ function miInformacion() {
   // TODO: Declara las variables nombre, edad y carrera
   // Ejemplo: const nombre = "Juan";
   
-  const nombre = "";
-  const edad = 0;
-  const carrera = "";
+  const nombre = "asd";
+  const edad = 10;
+  const carrera = "dfdf";
   
   return { nombre, edad, carrera };
 }
